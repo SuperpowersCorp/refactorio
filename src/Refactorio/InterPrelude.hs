64bed544658b451f03b5b27124b1b4c590e344c5
John Evans <lgastako@gmail.com>
2018-06-03 04:48:00 -0600

    Nothing works.  Grrr.

--- src/Refactorio/InterPrelude.hs
+++ src/Refactorio/InterPrelude.hs
@@ -7,16 +7,33 @@ import Data.Data.Lens
 import Language.Haskell.Exts
 import Language.Haskell.Exts.Prisms
 
+anyModuleNameL :: Traversal' (Module SrcSpanInfo) SrcSpan
+anyModuleNameL = _Module . biplate . _ModuleName . _1 . srcSpanInfoL
+
 end :: (Functor f, Field1 s t a b) => (a -> f b) -> s -> f t
 end = _1
+
+moduleNameL :: Traversal' (Module SrcSpanInfo) SrcSpan
+moduleNameL = _Module._2._Just._ModuleHead._2._ModuleName._1.srcSpanInfoL
 
 srcSpanInfoL :: Lens' SrcSpanInfo SrcSpan
 srcSpanInfoL = lens srcInfoSpan $ \ssi sis -> ssi { srcInfoSpan = sis }
 
-moduleNameL :: Applicative f
-            => LensLike f (Module SrcSpanInfo) (Module SrcSpanInfo) SrcSpan SrcSpan
-moduleNameL = _Module._2._Just._ModuleHead._2._ModuleName._1.srcSpanInfoL
+integersL :: Traversal' (Module SrcSpanInfo) Int
+integersL = _Module.biplate._Int.end
 
-anyModuleNameL :: Applicative f
-               => LensLike f (Module SrcSpanInfo) (Module SrcSpanInfo) SrcSpan SrcSpan
-anyModuleNameL = _Module . biplate . _ModuleName . _1 . srcSpanInfoL
+-- integersL :: Applicative f
+--           => LensLike f (Module SrcSpanInfo) (Module SrcSpanInfo) Int Int
+-- integersL = _Module.biplate._Int.end
+
+foo :: Traversal' (Module SrcSpanInfo) String
+foo = _Module.biplate._String.end
+
+stringsL :: Traversal' (Module SrcSpanInfo) String
+stringsL = _Module.biplate._String.end
+
+-- TODO: Why doesn't this work?
+-- rationalsL :: Traversal' (Module SrcSpanInfo) Rational
+-- rationalsL = _Module.biplate._Frac.end
+
+deleteMe = 5.3
