7f7fb0a2d8dee9d63eeada0275abf582f950b67f
John Evans <lgastako@gmail.com>
2018-06-19 00:58:07 -0600

    Remove dangling unqualifieds.

--- src/X/Language/Haskell/Interpreter.hs
+++ src/X/Language/Haskell/Interpreter.hs
@@ -30,8 +30,7 @@ build possiblePreludes src = runInterpreter $ do
   let preludeImport = maybe [] importPrelude . head $ possiblePreludes
   putLn $ "DEBUG: preludeImport: " <> show preludeImport
   setImportsQ $
-    [ ("Codec.Compression.Zlib.Lens" , Nothing)
-    , ("Control.Lens"                , Just "L")
+    [ ("Control.Lens"                , Just "L")
     , ("Data.Char"                   , Just "Char")
     , ("Data.String"                 , Just "String")
     , ("Data.Text"                   , Just "Text")
@@ -49,23 +48,13 @@ build possiblePreludes src = runInterpreter $ do
     , ("Text.Pandoc.Lens"            , Just "Pandoc")
     , ("Text.Regex.Lens"             , Just "R")
     , ("Text.Regex.Lens"             , Just "Regex")
-    , ("Text.Regex.Quote"            , Nothing)
+    , ("Text.Regex.Quote"            , Nothing)       -- The only unqualified exception
     , ("Text.Xml.Lens"               , Just "Html")
     , ("Text.Xml.Lens"               , Just "H")
     , ("Text.Xml.Lens"               , Just "Xml")
     , ("Text.Xml.Lens"               , Just "X")
     ] ++ preludeImport
   interpret (unpack ("(" <> src <> ")")) infer
-
--- We only import qualified stuff now and the only unqualified stuff comes from
--- whatever Prelude is resolved.
---
---   ("Control.Lens"                , Nothing)
--- , ("Data.ByteString.Lens"        , Nothing)
--- , ("Data.Data.Lens"              , Nothing)
--- , ("Refactorio.Prelude"          , Nothing)
--- , ("Text.Regex.Quote"            , Nothing)
--- , ("Codec.Compression.Zlib.Lens" , Nothing)
 
 importPrelude :: String -> [(String, Maybe String)]
 importPrelude s = [(s, Nothing)]
