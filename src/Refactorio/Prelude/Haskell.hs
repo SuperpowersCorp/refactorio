b9548e8f1b1d0eea8028bbf4096c95760c258012
John Evans <lgastako@gmail.com>
2018-06-21 23:54:26 -0600

    Fix recursive legacy search bug and add 'target' helper in Haskell prelude.

--- src/Refactorio/Prelude/Haskell.hs
+++ src/Refactorio/Prelude/Haskell.hs
@@ -3,6 +3,7 @@
 module Refactorio.Prelude.Haskell
      ( module Exports
      , end
+     , target
      ) where
 
 import Refactorio.Prelude.Basic       as Exports hiding ( Alt
@@ -21,3 +22,6 @@ import X.Language.Haskell.Exts.Prisms as Exports
 
 end :: Field1 s t a b => Lens s t a b
 end = _1
+
+target :: (Functor f, Field2 s t a b) => (a -> f b) -> s -> f t
+target = _2
